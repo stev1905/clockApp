ed414a11f61c104be453fa0e8c4d78cbd760e65a
Christian Stevens <stev1905@gmail.com>
2020-08-11 12:31:58 -0400

    added font styling flexbox and static html

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-var today = new Date();
-var hours = today.getHours();
-var minutes = today.getMinutes();
-var seconds = today.getSeconds();
-var day = today.getDay();
-var aMpM = 'AM';
-var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+function myTimer() {
+    var today = new Date();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+    var day = today.getDay();
+    var aMpM = 'AM';
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var dayOfWeek = days[today.getDay()];
+    var month = months[today.getMonth()];
 
-var dayOfWeek = days[today.getDay()];
-var month = months[today.getMonth()];
+    if(hours > 12) {
+        hours = hours - 12;
+        aMpM = 'PM';
+    }
+    if(seconds < 10) {
+        seconds = '0'+ seconds;
+    }
 
-if(hours > 12) {
-    hours = hours - 12;
-    aMpM = 'PM';
+    var currentTime = hours + ":" + minutes + ":" + seconds +' '+aMpM;
+    var currentDate = dayOfWeek + "-" + month +" "+ day;
+
+    document.getElementsByClassName('time')[0].innerText = currentTime;
+    document.getElementsByClassName('date')[0].innerText = currentDate;
 }
-
-var currentTime = hours + ":" + minutes + ":" + seconds +' '+aMpM;
-var currentDate = dayOfWeek + "-" + month +" "+ day;
-
-document.getElementsByClassName('time')[0].innerText = currentTime;
-document.getElementsByClassName('date')[0].innerText = currentDate;
+var myVar = setInterval(function() {
+    myTimer();
+  }, 1000);
